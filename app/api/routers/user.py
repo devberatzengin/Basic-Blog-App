d5a8807c9cd7894f9d967238a60b38afed75831c
Berat Zengin <devberatzengin@gmail.com>
2026-01-31 21:15:26 +0300

    feat: endpoint update for like and post counts

--- app/api/routers/user.py
+++ app/api/routers/user.py
@@ -1,9 +1,20 @@
 from fastapi import APIRouter, Depends
 from app.api.deps import get_current_user
 from app.schemas.user import UserOut
+from sqlalchemy.orm import Session
+from app.database.database import get_db
+from app.models.post import Post
+from app.models.follow import Follow  
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.get("/me", response_model=UserOut)
-def get_my_profile(current_user = Depends(get_current_user)):
+def get_my_profile( db: Session = Depends(get_db), current_user = Depends(get_current_user) ):
+    post_count = db.query(Post).filter(Post.user_id == current_user.id).count()
+    
+    follower_count = db.query(Follow).filter(Follow.following_id == current_user.id).count()
+
+    current_user.post_count = post_count
+    current_user.follower_count = follower_count
+    
     return current_user
