ad00f6d94c124779f9a60b4663883b6021dac6ff
Berat Zengin <devberatzengin@gmail.com>
2026-01-31 21:19:42 +0300

    feat: token mothods added

--- app/core/security.py
+++ app/core/security.py
@@ -1,13 +1,35 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
+from app.core.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# Password Methods
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
+
+# Jwt Token Methods
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("user_id")
+        if user_id is None:
+            return None
+        return user_id
+    except JWTError:
+        return None
