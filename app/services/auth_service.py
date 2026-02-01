52bd7348682be4171fc2cb1f24fdfb657bdc853c
Berat Zengin <devberatzengin@gmail.com>
2026-02-01 14:45:10 +0300

    fix: user authenticate method fixed for auth

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,6 +1,17 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.core.security import hash_password, verify_password, create_access_token
+from jose import jwt
+from datetime import datetime, timedelta
+from app.core.security import SECRET_KEY, ALGORITHM
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
 
 def register_user(db: Session, email: str, password: str, first_name: str, last_name: str):
     user = User(
@@ -16,9 +27,13 @@ def register_user(db: Session, email: str, password: str, first_name: str, last_
 
 def authenticate_user(db: Session, email: str, password: str):
     user = db.query(User).filter(User.email == email).first()
-    if not user or not verify_password(password, user.hashed_password):
-        return None
-    return create_access_token({"user_id": user.id})
+    if not user:
+        return False
+    
+    if not verify_password(password, user.hashed_password):
+        return False 
+        
+    return user
 
 
 def get_user_by_id(db: Session, user_id: int):
