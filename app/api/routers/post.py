360b18de12a5724ec56604aad5f4b565c7b0874a
Berat Zengin <devberatzengin@gmail.com>
2026-02-01 13:35:35 +0300

    fix: router fixed for likedpost endpoint

--- app/api/routers/post.py
+++ app/api/routers/post.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session, joinedload
 from app.models.post import Post
 from app.models.like import Like
 from app.schemas.post import PostCreate, PostOut
+from typing import Optional
 from app.services.post_service import create_post, get_posts, delete_post as dp
 from app.api.deps import get_current_user
 from app.database.database import get_db
@@ -22,14 +23,23 @@ def list_posts(db: Session = Depends(get_db)):
     return get_posts(db)
 
 @router.get("/liked-posts", response_model=list[PostOut])
-def get_liked_posts(
-    db: Session = Depends(get_db),
+@router.get("/liked-posts/{user_id}", response_model=list[PostOut])
+def get_user_liked_posts(
+    user_id: Optional[int] = None, 
+    db: Session = Depends(get_db), 
     current_user = Depends(get_current_user)
 ):
-    liked_posts = db.query(Post).join(Like).filter(Like.user_id == current_user.id).all()
+
+    target_id = user_id if user_id is not None else current_user.id
+    
+    liked_posts = db.query(Post).join(Like).filter(Like.user_id == target_id).all()
     
     for post in liked_posts:
-        post.is_liked = True
+        like_exists = db.query(Like).filter(
+            Like.post_id == post.id, 
+            Like.user_id == current_user.id
+        ).first()
+        post.is_liked = True if like_exists else False
         
     return liked_posts
 
