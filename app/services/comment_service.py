521a35fc139edbe842a9461a691496233a113c67
Berat Zengin <devberatzengin@gmail.com>
2026-02-01 14:42:56 +0300

    feat: delete comment feat added

--- app/services/comment_service.py
+++ app/services/comment_service.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from app.models.comment import Comment 
 from app.schemas.comment import CommentCreate 
 
@@ -16,4 +16,17 @@ def create_comment(db: Session, comment_data: CommentCreate, current_user_id: in
     return new_comment
 
 def get_comments_by_post(db: Session, post_id: int):
-    return db.query(Comment).filter(Comment.post_id == post_id).all()
+    return db.query(Comment).options(joinedload(Comment.user)).filter(Comment.post_id == post_id).all()
+
+def delete_comment(db: Session, comment_id: int, user_id: int):
+    comment = db.query(Comment).filter(Comment.id == comment_id).first()
+    
+    if not comment:
+        return False 
+    
+    if comment.user_id != user_id:
+        return None # return 403
+
+    db.delete(comment)
+    db.commit()
+    return True 
